2afd16a10a84ec6aee00bef5afd5a154d17d3123
Ilja Everilä <saarni@gmail.com>
2012-10-12 14:25:55 +0300

    Be a little less verbose

--- src/midi.cpp
+++ src/midi.cpp
@@ -57,14 +57,14 @@ _Midi::Handlers::CallbackPtr _Midi::getControlChangeHandler() {
 	// first data byte has controller number
 #if MIDI_CHANNEL_MODE
 	switch (dataBuffer[0]) {
-		case Messages::ChannelMode::AllSoundOff:         handler = handlers.allSoundOff;         break;
-		case Messages::ChannelMode::ResetAllControllers: handler = handlers.resetAllControllers; break;
-		case Messages::ChannelMode::LocalControl:        handler = handlers.localControl;        break;
-		case Messages::ChannelMode::AllNotesOff:         handler = handlers.allNotesOff;         break;
-		case Messages::ChannelMode::OmniModeOff:         handler = handlers.omniModeOff;         break;
-		case Messages::ChannelMode::OmniModeOn:          handler = handlers.omniModeOn;          break;
-		case Messages::ChannelMode::MonoModeOn:          handler = handlers.monoModeOn;          break;
-		case Messages::ChannelMode::PolyModeOn:          handler = handlers.polyModeOn;          break;
+		case Messages::AllSoundOff:         handler = handlers.allSoundOff;         break;
+		case Messages::ResetAllControllers: handler = handlers.resetAllControllers; break;
+		case Messages::LocalControl:        handler = handlers.localControl;        break;
+		case Messages::AllNotesOff:         handler = handlers.allNotesOff;         break;
+		case Messages::OmniModeOff:         handler = handlers.omniModeOff;         break;
+		case Messages::OmniModeOn:          handler = handlers.omniModeOn;          break;
+		case Messages::MonoModeOn:          handler = handlers.monoModeOn;          break;
+		case Messages::PolyModeOn:          handler = handlers.polyModeOn;          break;
 		default:
 #else
 	if (databuffer[0] < 120) {
@@ -87,34 +87,34 @@ void _Midi::messageHandler(uint8_t status) {
 	Handlers::CallbackPtr handler = nullptr;
 
 	switch (denseIndexFromStatus(status)) {
-		case denseIndexFromStatus(Messages::ChannelVoice::NoteOff):               handler = handlers.noteOff;               break;
-		case denseIndexFromStatus(Messages::ChannelVoice::NoteOn):                handler = handlers.noteOn;                break;
-		case denseIndexFromStatus(Messages::ChannelVoice::PolyphonicKeyPressure): handler = handlers.polyphonicKeyPressure; break;
-		case denseIndexFromStatus(Messages::ChannelVoice::ControlChange):         handler = getControlChangeHandler();      break;
-		case denseIndexFromStatus(Messages::ChannelVoice::ProgramChange):         handler = handlers.programChange;         break;
-		case denseIndexFromStatus(Messages::ChannelVoice::ChannelPressure):       handler = handlers.channelPressure;       break;
-		case denseIndexFromStatus(Messages::ChannelVoice::PitchWheelChange):      handler = handlers.pitchWheelChange;      break;
+		case denseIndexFromStatus(Messages::NoteOff):               handler = handlers.noteOff;               break;
+		case denseIndexFromStatus(Messages::NoteOn):                handler = handlers.noteOn;                break;
+		case denseIndexFromStatus(Messages::PolyphonicKeyPressure): handler = handlers.polyphonicKeyPressure; break;
+		case denseIndexFromStatus(Messages::ControlChange):         handler = getControlChangeHandler();      break;
+		case denseIndexFromStatus(Messages::ProgramChange):         handler = handlers.programChange;         break;
+		case denseIndexFromStatus(Messages::ChannelPressure):       handler = handlers.channelPressure;       break;
+		case denseIndexFromStatus(Messages::PitchWheelChange):      handler = handlers.pitchWheelChange;      break;
 #if MIDI_SYSTEM_COMMON
 // NOTE: disabling System Common, but enabling SystemRealTime will
 // make this switch/case sparse, could cause crappier compiler results?
-		case denseIndexFromStatus(Messages::SystemCommon::SystemExclusive):       handler = handlers.systemExclusive;      break;
-		case denseIndexFromStatus(Messages::SystemCommon::TimeCodeQuarterFrame):  handler = handlers.timeCodeQuarterFrame; break;
-		case denseIndexFromStatus(Messages::SystemCommon::SongPositionPointer):   handler = handlers.songPositionPointer;  break;
-		case denseIndexFromStatus(Messages::SystemCommon::SongSelect):            handler = handlers.songSelect;           break;
-		case denseIndexFromStatus(0xF4): /* Undefined */ break;
-		case denseIndexFromStatus(0xF5): /* Undefined */ break;
-		case denseIndexFromStatus(Messages::SystemCommon::TuneRequest):           handler = handlers.tuneRequest;    break;
-		case denseIndexFromStatus(Messages::SystemCommon::EndOfExclusive):        handler = handlers.endOfExclusive; break;
+		case denseIndexFromStatus(Messages::SystemExclusive):       handler = handlers.systemExclusive;       break;
+		case denseIndexFromStatus(Messages::TimeCodeQuarterFrame):  handler = handlers.timeCodeQuarterFrame;  break;
+		case denseIndexFromStatus(Messages::SongPositionPointer):   handler = handlers.songPositionPointer;   break;
+		case denseIndexFromStatus(Messages::SongSelect):            handler = handlers.songSelect;            break;
+		case denseIndexFromStatus(0xF4):                            /* Undefined */                           break;
+		case denseIndexFromStatus(0xF5):                            /* Undefined */                           break;
+		case denseIndexFromStatus(Messages::TuneRequest):           handler = handlers.tuneRequest;           break;
+		case denseIndexFromStatus(Messages::EndOfExclusive):        handler = handlers.endOfExclusive;        break;
 #endif
 #if MIDI_SYSTEM_REAL_TIME
-		case denseIndexFromStatus(Messages::SystemRealTime::TimingClock):         handler = handlers.timingClock; break;
-		case denseIndexFromStatus(0xF9): /* Undefined */ break;
-		case denseIndexFromStatus(Messages::SystemRealTime::Start):               handler = handlers.start;     break;
-		case denseIndexFromStatus(Messages::SystemRealTime::Continue):            handler = handlers.continue_; break;
-		case denseIndexFromStatus(Messages::SystemRealTime::Stop):                handler = handlers.stop;      break;
-		case denseIndexFromStatus(0xFD): /* Undefined */ break;
-		case denseIndexFromStatus(Messages::SystemRealTime::ActiveSensing):       handler = handlers.activeSensing; break;
-		case denseIndexFromStatus(Messages::SystemRealTime::Reset):               handler = handlers.reset;         break;
+		case denseIndexFromStatus(Messages::TimingClock):           handler = handlers.timingClock;           break;
+		case denseIndexFromStatus(0xF9):                            /* Undefined */                           break;
+		case denseIndexFromStatus(Messages::Start):                 handler = handlers.start;                 break;
+		case denseIndexFromStatus(Messages::Continue):              handler = handlers.continue_;             break;
+		case denseIndexFromStatus(Messages::Stop):                  handler = handlers.stop;                  break;
+		case denseIndexFromStatus(0xFD):                            /* Undefined */                           break;
+		case denseIndexFromStatus(Messages::ActiveSensing):         handler = handlers.activeSensing;         break;
+		case denseIndexFromStatus(Messages::Reset):                 handler = handlers.reset;                 break;
 #endif
 	}
 
